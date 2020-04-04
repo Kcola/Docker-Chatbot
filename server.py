ba6b111556556a069d79a85067af1d4476a80f16
Kolawole Campbell <kcampbell@becht.com>
2020-04-04 10:08:11 -0400

    optimizations

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#main.py
+# main.py
 from flask import Flask
 from flask import request, render_template, jsonify
 from convobot import ConvoBot
@@ -9,25 +9,28 @@ cb = ConvoBot()
 CORS(app, support_credentials=True)
 @app.route('/')
 def hello_world():
-    #cb.send('hi')
-    #return cb.reply()
+    # cb.send('hi')
+    # return cb.reply()
     pass
+
+
 @app.route('/send', methods=['GET', 'POST'])
 def send():
     if request.method == 'POST':
         user_in = json.loads(request.data)
-        
+
         cb.send(user_in['chat'])
-            
+
         bot_rep = cb.reply()
         bot_rep = {
-        "bot_response":bot_rep
+            "bot_response": bot_rep
         }
         return bot_rep
-        
-        #return render_template('reply.html', user_in=user_in, bot_rep=bot_rep)
-    
+
+        # return render_template('reply.html', user_in=user_in, bot_rep=bot_rep)
+
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
